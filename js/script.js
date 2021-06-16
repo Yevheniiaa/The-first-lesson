b9c06be4372ad00ae2c393e73d58d3be53044e53
Yevheniia <jane4950@gmail.com>
2021-06-16 20:22:46 +0300

    PersonalMovieDB

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 
-"use srtict"
+"use strict";
 
-console.log("hello");
+const numberOfFiles = +prompt("How many movies have you already seen?", "0");
+const personalMovieDB = {
+    count: numberOfFiles,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
 
-let number = 5;
-number = 10;
-console.log(number);
+
+const a = prompt("What's the last movie you watched?", "");  
+const b = prompt("How do you score this movie?", "");
+const c = prompt("What's the last movie you watched?", "");  
+const d = prompt("How do you score this movie?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
