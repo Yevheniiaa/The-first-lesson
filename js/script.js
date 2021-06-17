48c32f3db40a2c784a720bd2ada67c8b8d16e6e5
Yevheniia <jane4950@gmail.com>
2021-06-17 23:52:50 +0300

    PersonalMovieDB

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
 "use strict";
 
-const numberOfFiles = +prompt("How many movies have you already seen?", "0");
-const personalMovieDB = {
+ const numberOfFiles = +prompt("How many movies have you already seen?", "0");
+ const personalMovieDB = {
     count: numberOfFiles,
     movies: {},
     actors: {},
@@ -10,13 +10,72 @@ const personalMovieDB = {
     privat: false,
 };
 
-
-const a = prompt("What's the last movie you watched?", "");  
-const b = prompt("How do you score this movie?", "");
-const c = prompt("What's the last movie you watched?", "");  
-const d = prompt("How do you score this movie?", "");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt("What's the last movie you watched?", "");  
+    const b = prompt("How do you score this movie?", "");
+    
+    
+    if (a == null || b == null || a.length > 50 || a == "" || b == "") {
+        i--;
+        console.log("error");
+    } else {
+        personalMovieDB.movies[a] = b;   
+        console.log("done");
+    }
+}
 
 console.log(personalMovieDB);
+
+if (numberOfFiles < 10) {
+        console.log("You have seen quite a few movies!");
+    } else if (numberOfFiles <= 30 && numberOfFiles >= 10) {
+        console.log("You are a classical viewer!"); 
+    } else if (numberOfFiles > 30) {
+        console.log("You a real movie fan");
+    } else {
+        console.log("Error");
+}
+/*let i = 0;
+while (i < 2) {
+    i++;
+    const a = prompt("What's the last movie you watched?", "");  
+    const b = prompt("How do you score this movie?", "");
+        
+    if (a == null || b == null || a.length > 50 || a == "" || b == "") {
+        i--;
+        console.log("error");
+    } else {
+        personalMovieDB.movies[a] = b;   
+        console.log("done");
+    }
+}
+console.log(personalMovieDB);
+   
+let i = 0;
+do {
+    i++;
+    const a = prompt("What's the last movie you watched?", "");  
+    const b = prompt("How do you score this movie?", "");
+    
+    if (a == null || b == null || a.length > 50 || a == "" || b == "") {
+        i--;
+        console.log("error");
+    } else {
+        personalMovieDB.movies[a] = b;   
+        console.log("done");
+    }
+    
+} while (i < 2);
+console.log(personalMovieDB);
+
+
+
+
+
+for (let i = 0; i < 40; i++) {
+    if (i == 35) {
+           break;
+    }
+    console.log(i);
+}
+*/
